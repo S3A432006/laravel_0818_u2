cb4deff057b4dda8e78f2b608a933cad05d51cda
JIA YI LAI <anniuu7134@gmail.com>
2018-01-15 01:58:12 +0800

    設定Route

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/home',function(){
-       return view('home.index');
-  });
+
+Route::get('/home', ['as' => 'home.index', 'uses' => 'HomeController@index']);
+Route::get('hello/{name?}', ['as' => 'hello.index', 'uses' => 'HelloController@index']);
